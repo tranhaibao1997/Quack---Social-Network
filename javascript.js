7c2942ae8fffd8e26fbbd8d4194d66657118299d
Tran Hai Bao <tranhaibao1997@gmail.com>
2020-05-23 14:24:00 +0700

    add js

--- javascript.js
+++ javascript.js
@@ -1,118 +1,319 @@
-let maxChar = 140;
-let message = document.getElementById("input")
-let quackMessage = []
-let num = 0
+let quackList = JSON.parse(localStorage.getItem('session')) || []
+let maxInput = 100;
+let username = "bitna"
+let avatar = "https://www.catprotection.com.au/site/wp-content/uploads/2019/03/cat-banner.jpg"
 
-message.addEventListener("input", remainder)
 
-function remainder() {
-    let text = event.target.value
-    let remainText = maxChar - text.length
-    document.getElementById("remain").innerHTML = `${remainText} characters left`
-    if (text.length > 140 || text.length <= 0) {
-        document.getElementById("btn-post").disabled = true
-    } else {
-        document.getElementById("btn-post").disabled = false
-    }
-}
 
-function post() {
-    let remainText = maxChar - event.target.value.length
-    message = document.getElementById("input").value
-    let quack = {
-        'content': findHashtag(message),
-        'id': num,
-        'isLike': false,
-        'isRetweet': false,
-        'parents': null,
-    }
-    num++
-    quackMessage.push(quack)
-    console.log(quackMessage)
-    render(quackMessage)
-    document.getElementById("input").value = ''
-    document.getElementById("remain").innerHTML = `${remainText} characters left`
+// let username = "haibaotran"
+// let avatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTI9luOzrxxgQ_CTWLAbTEEp_b-CYFSFllZTeKxUEI6Z4jYKBTq&usqp=CAU"
 
-}
 
-function toggleLike(position) {
-    if (quackMessage[position].isLike == false) {
-        quackMessage[position].isLike = true
-    } else {
-        quackMessage[position].isLike = false
-    }
-    console.log(quackMessage[position])
-    render(quackMessage)
-}
+function getTrending() {
 
-const render = (list) => {
-    console.log(list)
-    let quackHtml = list.map((item, index) => {
-        if (item.isLike == false) {
-            return `<div class="qAreaFormat">
-            ${item.content}
-            <div>
-            <span onclick="toggleLike(${index})"><i class="far fa-star fa-lg iconFormat star"></i></span>
-            <span onclick="retweet(${index})"><i class="fas fa-retweet fa-lg iconFormat retweet"></i></span>
-            <span><i class="far fa-comment fa-lg iconFormat comment"></i></span>
-            <span onclick="remove(${index})"><i class="fas fa-trash-alt fa-lg iconFormat trash"></i></span>
-            </div>
-            </div>`
-        } else {
-            return `<div class="qAreaFormat">
-            ${item.content}
-            <div>
-            <span onclick="toggleLike(${index})"><i class="fas fa-star fa-lg iconFormat star"></i></span>
-            <span onclick="retweet(${index})"><i class="fas fa-retweet fa-lg iconFormat retweet"></i></span>
-            <span onclick="remove(${index})"><i class="fas fa-trash-alt fa-lg iconFormat trash"></i></span>
-            </div>
-            </div>`
+    let testArray = []
+    let hashTagArray = quackList.map(quack => {
+        for (let i = 0; i < quack.hashTag.length; i++) {
+            testArray.push(quack.hashTag[i])
         }
     })
-    document.getElementById("quackArea").innerHTML = quackHtml.join(" ")
+    let obj1 = {};
+    for (let i = 0; i < testArray.length; i++) {
+        let name = testArray[i];
+        if (!obj1[name]) {
+            obj1[name] = 1;
+        } else {
+            obj1[name]++;
+        }
+    }
+    let myArray = Object.keys(obj1)
+    console.log(myArray)
+    let html = myArray.map((item, index) => (`<li><a href="#" data-hashtag=${item} onclick="filterHashTag(event)">${item}</a></li>`)).join("")
+    document.getElementById("trending-section").innerHTML = html
+}
+getTrending()
+
+
+
+
+
+
+
+renderList(quackList)
+    //render textarea
+function rederTextArea() {
+    return `
+
+ <img src="${avatar}" width="100px" height="100px">
+ <div>
+ <p>Hello <a href="#">${username}</a></p>
+ <textarea type="text" placeholder="Input tweet" id="input"></textarea>
+ <button id="btn-post" onclick="post()">Quack</button>
+ </div>
+
+ `
+}
+document.getElementById("input-area").innerHTML = rederTextArea()
+
+
+//check textarea input
+document.getElementById("input").addEventListener("input", (event) => {
+    let message = event.target.value
+    if (event.target.value == "#") {
+
+    }
+    let textRemain = maxInput - message.length
+    document.getElementById("remain").innerHTML = `textRemain:${textRemain}`
+    if (textRemain < 0) {
+        document.getElementById("remain").style = "color:red"
+        document.getElementById("remain").innerHTML = "Your Quack is too long"
+        document.getElementById("btn-post").disabled = true
+    } else {
+        document.getElementById("remain").innerHTML = `textRemain:${textRemain}`
+        document.getElementById("remain").style = "color:black"
+        document.getElementById("btn-post").disabled = false
+    }
+})
+
+function post() {
+    let message = document.getElementById("input").value
+    let hashTag = [];
+    let peopleTag = [];
+    let likeBy = [];
+    let comments = [];
+    let newMessage = message.split(" ").map((elm, index) => {
+        if (elm.startsWith("#")) {
+            hashTag.push(elm)
+            return `<a href="#" data-hashtag=${elm} onclick="filterHashTag(event)">${elm}</a>`;
+        } else if (elm.startsWith("@")) {
+            peopleTag.push(elm)
+            return `<a href="#" onclick="filterPeopleTag(${elm})">${elm}</a>`;
+        }
+        return elm;
+    }).join(" ");
+
+    let quack = {
+        "username": username,
+        "avatar": avatar,
+        "content": newMessage,
+        "isReQuacked": false,
+        "isLiked": false,
+        "reQuackLevel": 0,
+        "likeBy": likeBy,
+        "hashTag": hashTag,
+        "peopleTag": peopleTag,
+        "comments": comments
+    }
+    quackList.unshift(quack)
+    renderList(quackList)
+    updateLocalStorage(quackList)
+    getTrending()
 }
 
-function remove(index) {
-    let deletedNumber=quackMessage[index].id
-    let newArray=quackMessage.filter(elm =>{
-        if(elm.id===deletedNumber || elm.parents===deletedNumber)
-        {
+function renderList(list) {
+    function renderElm(elm, index) {
+        console.log(elm)
+        if (elm.likeBy.findIndex(name => name == username) !== -1 && elm.username == username) {
+            //if user liked and this post belong to user
+            console.log(" //if user liked and this post belong to user")
+            console.log(elm.likeBy)
+            return `
+            <div class="quack">
+            <img src="${elm.avatar}" width="100px" height="100px">
+            <div>
+             <a href="#">@${elm.username}</a>
+            <p>${elm.content}</p>
+            </div>
+            <button onclick="deleteQuack(${index})">Delete</button>
+            <button onclick="editQuack(${index})">Edit</button>
+            <button onclick="reQuack(${index})">Retweet</button>
+            <button onclick="comments(${index})">Comment</button>
+            <button onclick="likeTweet(${index})"><i class="fas fa-star"></i><span>${elm.likeBy.length}</span></button>
+            <input class="comment-box" type="text" placeholder="add comment...">
+            <button onclick="postComment(${index})">Post Comment</button>
+            ${renderComments(elm.comments)}
+            </div>
+          
+      
+          
+            `
+        } else if (elm.likeBy.findIndex(name => name == username) === -1 && elm.username == username) {
+            //if user dont like and this post belong to user
+            console.log("//if user dont like and this post belong to user")
+            return `
+            <div class="quack">
+            <img src="${elm.avatar}" width="100px" height="100px">
+            <div>
+             <a href="#">@${elm.username}</a>
+            <p>${elm.content}</p>
+            </div>
+            <button onclick="deleteQuack(${index})">Delete</button>
+            <button onclick="editQuack(${index})">Edit</button>
+            <button onclick="reQuack(${index})">Retweet</button>
+            <button onclick="comments(${index})">Comment</button>
+            <button onclick="likeTweet(${index})"><i class="far fa-star"></i><span>${elm.likeBy.length}</span></button>
+            <input class="comment-box" type="text" placeholder="add comment...">
+            <button onclick="postComment(${index})">Post Comment</button>
+            ${renderComments(elm.comments)}
+            </div>
+            `
+        } else if (elm.likeBy.findIndex(name => name == username) !== -1 && elm.username != username) {
+            //if user liked but this post dont belong to user
+            console.log(" //if user liked but this post dont belong to user")
+            return `
+            <div class="quack">
+            <img src="${elm.avatar}" width="100px" height="100px">
+            <div>
+             <a href="#">@${elm.username}</a>
+            <p>${elm.content}</p>
+            </div>
+            <button onclick="reQuack(${index})">Retweet</button>
+            <button onclick="comments(${index})">Comment</button>
+            <button onclick="likeTweet(${index})"><i class="fas fa-star"></i><span>${elm.likeBy.length}</span></button>
+            <input class="comment-box" type="text" placeholder="add comment...">
+            <button onclick="postComment(${index})">Post Comment</button>
+            ${renderComments(elm.comments)}
+            </div>
+            `
+
+        } else if (elm.likeBy.findIndex(name => name == username) !== -1 && elm.username !== username) {
+            //if user dont link and this post dont belong to user
+            console.log(" if user dont link and this post dont belong to user")
+            return `
+            <div class="quack">
+            <img src="${elm.avatar}" width="100px" height="100px">
+            <div>
+             <a href="#">@${elm.username}</a>
+            <p>${elm.content}</p>
+            </div>
+            <button onclick="reQuack(${index})">Retweet</button>
+            <button onclick="comments(${index})">Comment</button>
+            <button onclick="likeTweet(${index})"><i class="far fa-star"></i><span>${elm.likeBy.length}</span></button>
+            <input class="comment-box" type="text" placeholder="add comment...">
+            <button onclick="postComment(${index})">Post Comment</button>
+            ${renderComments(elm.comments)}
+            </div>
+            `
+        } else {
+            //if user dont link and this post dont belong to user
+            console.log(" if user dont link and this post dont belong to user")
+            return `
+ <div class="quack">
+ <img src="${elm.avatar}" width="100px" height="100px">
+ <div>
+  <a href="#">@${elm.username}</a>
+ <p>${elm.content}</p>
+ </div>
+ <button onclick="reQuack(${index})">Retweet</button>
+ <button onclick="comments(${index})">Comment</button>
+ <button onclick="likeTweet(${index})"><i class="far fa-star"></i><span>${elm.likeBy.length}</span></button>
+ <input class="comment-box" type="text" placeholder="add comment...">
+ <button onclick="postComment(${index})">Post Comment</button>
+ ${renderComments(elm.comments)}
+ </div>
+ `
+        }
+
+
+    }
+    let html = list.map(renderElm).join("")
+    document.getElementById("quackArea").innerHTML = html
+
+
+}
+
+function deleteQuack(index) {
+    console.log(quackList[index].reQuackLevel)
+    let newlist = quackList.filter(quack => {
+        if (quackList[index].content == quack.content && quackList[index].reQuackLevel <= quack.reQuackLevel) {
             return false
         }
         return true
     })
-    quackMessage=newArray
-    render(quackMessage)
-
+    console.log(newlist)
+    quackList = newlist
+    renderList(quackList)
+    updateLocalStorage(quackList)
+    getTrending()
 }
 
-function retweet(position) {
+function likeTweet(index) {
+    if (quackList[index].likeBy.findIndex(name => name == username) !== -1) {
+        let likedUserNameIndex = quackList[index].likeBy.findIndex(name => name == username)
+        quackList[index].likeBy.splice(likedUserNameIndex, 1)
 
-    let quack = {
-        'id': num,
-        'content': quackMessage[position].content,
-        'isLike': false,
-        'isRetweet': true,
-        'parents': quackMessage[position].id
+    } else {
+        quackList[index].likeBy.push(username)
+        renderList(quackList)
     }
-    quackMessage[position].isRetweet = true
-    console
-    console.log("erfgtj", quackMessage)
-    quackMessage.push(quack)
-    render(quackMessage)
-    num++
+    renderList(quackList)
+    updateLocalStorage(quackList)
+
 }
 
-function findHashtag(message) {
-    let Array = message.split(" ").map(elm => {
-        if (elm.startsWith("#")) {
-            return `<a href="#">${elm}</a>`
-        } else if (elm.startsWith("@")) {
-            return `<span class="blue">${elm}</span>`
-        } else if (elm.startsWith("https://")) {
-            return `<img src="${elm}" width="300px">`
+function reQuack(index) {
+    let quack = {
+        "username": username,
+        "avatar": avatar,
+        "content": quackList[index].content,
+        "isReQuacked": true,
+        "isLiked": false,
+        "reQuackLevel": (quackList[index].reQuackLevel) + 1,
+        "likeBy": [],
+        "hashTag": quackList[index].hashTag,
+        "peopleTag": quackList[index].peopleTag,
+        "comments": []
+    }
+    quackList.unshift(quack)
+    renderList(quackList)
+    updateLocalStorage(quackList)
+    getTrending()
+}
+
+function filterHashTag(e) {
+    let hashtag = e.target.attributes["data-hashtag"].nodeValue
+    let hashTagList = quackList.filter(quack => {
+        if (quack.hashTag.find(elm => elm == hashtag)) {
+            return true;
         }
-        return elm
     })
-    return Array.join(" ")
+    renderList(hashTagList)
+}
+
+function updateLocalStorage(list) {
+    var a = [];
+    a = JSON.parse(localStorage.getItem('session')) || [];
+    a = list
+    localStorage.setItem('session', JSON.stringify(a));
+
+}
+
+function comments(index) {
+    let allCommentBoxes = document.querySelectorAll(".comment-box")
+    allCommentBoxes[index].style = "display:block"
+
+}
+
+function postComment(index) {
+    let allCommentBoxes = document.querySelectorAll(".comment-box")
+    let comment = {
+        content: allCommentBoxes[index].value,
+        username: username
+    }
+    quackList[index].comments.push(comment)
+    allCommentBoxes[index].style = "display:none"
+    updateLocalStorage(quackList)
+    console.log(quackList, "after post")
+    renderList(quackList)
+}
+
+function renderComments(list) {
+    function renderComment(elm) {
+        console.log(elm)
+        return ` <p>${elm.username}:${elm.content}</p>`
+    }
+    const commentNode = list.map(renderComment).join("")
+    return commentNode
+
+
 }
