feae83dcb7d45c58ec6459b403f49111a74f22a7
Hai Bao Tran <tranhaibao1997@gmail.com>
2020-05-24 14:05:23 +0700

    add dumb data

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let quackList = []
+let quackList = [{"id":50335323.482329294,"username":"Bitna","avatar":"https://zicxa.com/hinh-anh/wp-content/uploads/2020/02/T%E1%BB%95ng-h%E1%BB%A3p-h%C3%ACnh-%E1%BA%A3nh-avatar-h%C3%A0i-h%C6%B0%E1%BB%9Bc-nh%C3%ACn-l%C3%A0-b%E1%BA%ADt-c%C6%B0%E1%BB%9Di-1.jpg","content":"<img src=\"https://i.pinimg.com/originals/ec/56/76/ec56767a310dd2ee771ccb38792c7465.jpg\" alt=\"image\" width=\"50px\" height=\"50px\">","isReQuacked":true,"isLiked":false,"reQuackLevel":1,"parents":88477462.37204169,"likeBy":["haibaotran"],"hashTag":[],"peopleTag":[],"comments":[{"content":"wow so cute","username":"haibaotran","avatar":"https://thumbs.dreamstime.com/b/happy-smiling-geek-hipster-beard-man-cool-avatar-geek-man-avatar-104871313.jpg"}],"originalUser":"Alyssa","originalUserAvatar":"https://img.lovepik.com/element/40125/9858.png_860.png","reQuackComment":" cute pic ^^"},{"id":88477462.37204169,"username":"Alyssa","avatar":"https://img.lovepik.com/element/40125/9858.png_860.png","content":"<img src=\"https://i.pinimg.com/originals/ec/56/76/ec56767a310dd2ee771ccb38792c7465.jpg\" alt=\"image\" width=\"50px\" height=\"50px\">","isReQuacked":false,"isLiked":false,"reQuackLevel":0,"likeBy":["Bitna","haibaotran"],"hashTag":[],"peopleTag":[],"comments":[{"content":"i love this bunny !!!","username":"haibaotran","avatar":"https://thumbs.dreamstime.com/b/happy-smiling-geek-hipster-beard-man-cool-avatar-geek-man-avatar-104871313.jpg"}],"author":"Alyssa","parents":null},{"id":60412626.14233207,"username":"haibaotran","avatar":"https://thumbs.dreamstime.com/b/happy-smiling-geek-hipster-beard-man-cool-avatar-geek-man-avatar-104871313.jpg","content":"<a href=\"#\" data-hashtag=#coderschool onclick=\"filterHashTag(event)\">#coderschool</a>   we have learnt for 4 months...","isReQuacked":false,"isLiked":false,"reQuackLevel":0,"likeBy":["Alyssa"],"hashTag":["#coderschool"],"peopleTag":[],"comments":[{"content":"i know... so fast right ?","username":"Alyssa","avatar":"https://img.lovepik.com/element/40125/9858.png_860.png"}],"author":"haibaotran","parents":null},{"id":99074726.06905025,"username":"Bitna","avatar":"https://zicxa.com/hinh-anh/wp-content/uploads/2020/02/T%E1%BB%95ng-h%E1%BB%A3p-h%C3%ACnh-%E1%BA%A3nh-avatar-h%C3%A0i-h%C6%B0%E1%BB%9Bc-nh%C3%ACn-l%C3%A0-b%E1%BA%ADt-c%C6%B0%E1%BB%9Di-1.jpg","content":"<a href=\"#\" data-hashtag=#share onclick=\"filterHashTag(event)\">#share</a> \nReact is an open-source JavaScript library for building user interfaces. It is maintained by Facebook","isReQuacked":false,"isLiked":false,"reQuackLevel":0,"likeBy":["Alyssa","haibaotran"],"hashTag":["#share"],"peopleTag":[],"comments":[{"content":"very helpful Bitna","username":"haibaotran","avatar":"https://thumbs.dreamstime.com/b/happy-smiling-geek-hipster-beard-man-cool-avatar-geek-man-avatar-104871313.jpg"}],"author":"Bitna","parents":null},{"id":50372102.325621106,"username":"haibaotran","avatar":"https://thumbs.dreamstime.com/b/happy-smiling-geek-hipster-beard-man-cool-avatar-geek-man-avatar-104871313.jpg","content":"<a href=\"#\" data-hashtag=#coding onclick=\"filterHashTag(event)\">#coding</a> <a href=\"#\" data-hashtag=#JS onclick=\"filterHashTag(event)\">#JS</a>  coding is so fun guys !","isReQuacked":true,"isLiked":false,"reQuackLevel":1,"parents":67424074.1691331,"likeBy":["Alyssa","haibaotran","Bitna"],"hashTag":["#coding","#JS"],"peopleTag":[],"comments":[{"content":"i love coding too","username":"Bitna","avatar":"https://zicxa.com/hinh-anh/wp-content/uploads/2020/02/T%E1%BB%95ng-h%E1%BB%A3p-h%C3%ACnh-%E1%BA%A3nh-avatar-h%C3%A0i-h%C6%B0%E1%BB%9Bc-nh%C3%ACn-l%C3%A0-b%E1%BA%ADt-c%C6%B0%E1%BB%9Di-1.jpg"}],"originalUser":"Alyssa","originalUserAvatar":"https://img.lovepik.com/element/40125/9858.png_860.png","reQuackComment":" let's code everybodyyyy"},{"id":67424074.1691331,"username":"Alyssa","avatar":"https://img.lovepik.com/element/40125/9858.png_860.png","content":"<a href=\"#\" data-hashtag=#coding onclick=\"filterHashTag(event)\">#coding</a> <a href=\"#\" data-hashtag=#JS onclick=\"filterHashTag(event)\">#JS</a>  coding is so fun guys !","isReQuacked":false,"isLiked":false,"reQuackLevel":0,"likeBy":["haibaotran","Bitna"],"hashTag":["#coding","#JS"],"peopleTag":[],"comments":[{"content":"yeah :D","username":"haibaotran","avatar":"https://thumbs.dreamstime.com/b/happy-smiling-geek-hipster-beard-man-cool-avatar-geek-man-avatar-104871313.jpg"}],"author":"Alyssa","parents":null}]
 let maxInput = 100;
 
 
@@ -31,7 +31,7 @@ function renderAccountList() {
     <span>${elm.username}</span>
     </li>`).join("")
     let html = ` 
-    <i class="fas fa-window-close" id="close-btn"></i>
+   
     <div class="current-account">
     <div class="img-wrapper">
         <img src="${currentAvatar}" alt="">
@@ -43,7 +43,7 @@ function renderAccountList() {
         ${list}
     </ul>
 </div>`
-    document.getElementById("change-account").innerHTML = html
+    document.getElementById("account-section").innerHTML = html
 }
 
 function loginAnother(event) {
@@ -53,8 +53,7 @@ function loginAnother(event) {
     rederTextArea()
     renderList(quackList)
     renderAccountList()
-    document.getElementById("change-account").style = "display:none";
-    document.getElementById("body-change").style = "display:none"
+  
 }
 
 function getTrending() {
